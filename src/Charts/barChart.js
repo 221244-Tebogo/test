02575db8e58343510015d7a49618851317ee6620
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2023-05-26 12:52:51 +0200

    commit

--- src/Charts/barChart.js
+++ src/Charts/barChart.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from "chart.js";
 import { Line } from 'react-chartjs-2';
 
+// Registering Chart.js elements and plugins
 ChartJS.register(
   ArcElement,
   CategoryScale,
@@ -13,77 +14,77 @@ ChartJS.register(
   Tooltip,
   Legend,
   Filler
-)
+);
 
 const LineChart = () => {
- const [apiData, setApiData] =useState([]);
- const [categoryName, setCategoryName] = useState("Loading")
-    //Below its a Variable with a limited number of 10 that will be passed  thru API
-
-    
-  const [chart, setChart] = useState([])
- const baseUrl = "https://fakestoreapi.com/products?limit=10";
-  //var proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  //var apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-
+  const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
-    
-    axios.get(baseUrl)
-    .then((res) =>{
-      console.log(res.data)
-      setChart(res.data)
-    })
-  }, [])
+    // Fetching data from the API
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products?limit=10");
+        const data = response.data.map((product) => ({
+          label: product.title,
+          data: [product.price],
+          fill: false,
+          borderColor: `rgba(255, ${Math.floor(Math.random() * 256)}, 132, 1)`,
+        }));
+        setChartData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  console.log("chart", chart);
+    fetchData();
+  }, []);
+
+  // Function to calculate price differences between consecutive data points
+  const calculatePriceDifferences = (data) => {
+    const priceDifferences = [];
+    for (let i = 1; i < data.length; i++) {
+      const currentPrice = data[i].data[0];
+      const previousPrice = data[i - 1].data[0];
+      const difference = currentPrice - previousPrice;
+      priceDifferences.push(difference);
+    }
+    return priceDifferences;
+  };
+
+  // Chart data and options
   const data = {
-    labels: ["Fjallraven - Foldsack", "Mens Casual Premium Slim Fit T-Shirts", "Mens Cotton Jacket", "Mens Casual Slim Fit"],
-    datasets: [{
-      label: "Products Sales",
-      //data: [140, 340, 0, 0] ,
-      data: chart.map(y => y.price) ,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
+    labels: chartData.map((product) => product.label),
+    datasets: [
+      {
+        label: 'Sales',
+        data: chartData.map((product) => product.data[0]),
+        fill: false,
+        borderColor: 'rgba(255, 99, 132, 1)',
+      },
+      {
+        label: 'Price Differences',
+        data: calculatePriceDifferences(chartData),
+        fill: false,
+        borderColor: 'rgba(54, 162, 235, 1)',
+      },
+    ],
   };
 
   const options = {
     maintainAspectRatio: false,
-    scales: {
-    },
+    scales: {},
     legend: {
       labels: {
         fontSize: 25,
       },
     },
-  }
+  };
 
   return (
     <div>
-      <Line
-        data={data}
-        height={400}
-        options={options}
-
-      />
+      <Line data={data} height={300} options={options} />
     </div>
-  )
-}
+  );
+};
 
-export default LineChart
+export default LineChart;
